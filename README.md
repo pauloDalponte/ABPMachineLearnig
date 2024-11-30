a391bd46756db752cf6b4b4c32f54f9afaef1033
Paulo Cesar Dal Ponte <paulocesardalponte@gmail.com>
2024-11-29 22:34:37 -0300

    api

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
 # ABPMachineLearnig
+
+# Tutorial início api Flask
+
+### 1 Iniciar Virtual Environment
+
+```bash
+// Windows
+python -m venv venv
+venv\Scripts\activate
+
+Linux & macOs
+python -m venv venv
+source venv/bin/activate
+```
+
+### 2 Instalar pacotes
+
+```bash
+pip install -r requirements.txt
+pip install mysqlclient
+pip install PyPDF2
+```
+
+
+### 3 Rodar servidor web
+
+```bash
+// Para rodar na porta padrão
+flask run --host=0.0.0.0
+
+// Para escolher a porta http
+flask run --host=0.0.0.0 --port 5001
+```
--- back/app.py
+++ back/app.py
@@ -1,24 +1,25 @@
 from flask import Flask, request, jsonify
 from joblib import load  
+from flask_cors import CORS 
 
 app = Flask(__name__)
+CORS(app) 
 
-modelo = load(r'C:\Users\paulocesar\Documents\Sistemas Bendo\ABPMachineLearnig\models\model_nota_REDACAO.pkl')
+modelo = load(r'C:\Users\DalpTecnologia\Documents\ABPMachineLearnig\models\model_nota_REDACAO.pkl')
 
 @app.route('/prever', methods=['POST'])
 def prever():
     dados = request.json
     try:
        
-        entrada = [
-            dados['idade'],
-            dados['salario'],
-            dados['experiencia']
-        ]
+        dados = request.json
+
+        if not isinstance(dados, dict):
+            return jsonify({'erro': 'Os dados enviados devem estar no formato JSON'}), 400
+
+        entrada = list(dados.values())
         
-    
         previsao = modelo.predict([entrada])
-        
      
         return jsonify({'nota': previsao[0]})
     except Exception as e:
