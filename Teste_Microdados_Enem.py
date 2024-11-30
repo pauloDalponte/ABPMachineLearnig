f02da3b4472f5a3623fe6af7f965c99c818c7ce7
Iuri de Lima Marques <iurilmarques@hotmail.com>
2024-11-29 23:22:35 -0300

    correcao ao carregar modelo

--- Teste_Microdados_Enem.py
+++ Teste_Microdados_Enem.py
@@ -15,8 +15,7 @@ columns = [
     "TP_ESCOLA", "TP_ENSINO", "IN_TREINEIRO", "Q001", "Q002", "Q003",
     "Q004", "Q005", "Q006", "Q007", "Q008", "Q009", "Q010", "Q011",
     "Q012", "Q013", "Q014", "Q015", "Q016", "Q017", "Q018", "Q019",
-    "Q020", "Q021", "Q022", "Q023", "Q024", "Q025", 
-    "NU_NOTA_CN", "NU_NOTA_CH", "NU_NOTA_LC", "NU_NOTA_MT"
+    "Q020", "Q021", "Q022", "Q023", "Q024", "Q025"
 ]
 dataframe = dataframe[columns]
 dataframe.dropna(inplace=True, how='any', axis='index')
--- back/app.py
+++ back/app.py
@@ -5,7 +5,7 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app) 
 
-modelo = load(r'C:\Users\DalpTecnologia\Documents\ABPMachineLearnig\models\model_nota_REDACAO.pkl')
+modelo, ref_cols, target = load(r'C:\Users\DalpTecnologia\Documents\ABPMachineLearnig\models\model_nota_REDACAO.pkl')
 
 @app.route('/prever', methods=['POST'])
 def prever():
@@ -18,8 +18,12 @@ def prever():
             return jsonify({'erro': 'Os dados enviados devem estar no formato JSON'}), 400
 
         entrada = list(dados.values())
+
+        X_new = entrada[ref_cols]
+        y_new = entrada[target]
         
-        previsao = modelo.predict([entrada])
+
+        previsao = modelo.predict([X_new])
      
         return jsonify({'nota': previsao[0]})
     except Exception as e:
